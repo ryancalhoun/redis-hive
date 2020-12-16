c5a2f2ef608035160f4b6b431c8a8f075e040ae0
Ryan Calhoun <ryan.calhoun@wwt.com>
2020-12-16 14:02:48 -0600

    Add an empty integration test step; modify start/stop scripts for greater flexibility

--- start.sh
+++ start.sh
@@ -7,9 +7,16 @@ for i in $(seq 1 $N); do
   PROXY=$(( 7000 + i ))
   CONTROL=$(( 3000 + i ))
   mkdir -p data/redis-$REDIS
-  redis-server <(sed 's/{{PORT}}/'$REDIS'/g' redis.conf.in)
+
+  if [[ ! -f data/redis-$REDIS/redis-$REDIS.pid ]]; then
+    redis-server <(sed 's/{{PORT}}/'$REDIS'/g' redis.conf.in)
+  fi
 
   mkdir -p data/hive-$CONTROL
-  ./bin/redis-hive $REDIS $PROXY $CONTROL $(seq 3001 $(( 3000 + N)) ) > data/hive-$CONTROL/hive.log 2>&1 &
-  echo $! > data/hive-$CONTROL/hive.pid
+
+  if [[ ! -f data/hive-$CONTROL/hive.pid ]] ||
+     [[ $(cat /proc/$(cat data/hive-$CONTROL/hive.pid)/comm 2>/dev/null) != redis-hive ]]; then
+    ./bin/redis-hive $REDIS $PROXY $CONTROL $(seq 3001 $(( 3000 + N)) ) > data/hive-$CONTROL/hive.log 2>&1 &
+    echo $! > data/hive-$CONTROL/hive.pid
+  fi
 done
--- stop.sh
+++ stop.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-for pid in data/redis-*/*.pid; do
-  kill $(cat $pid)
-done
-for pid in data/hive-*/*.pid; do
-  kill $(cat $pid)
-done
+if [[ $1 == all ]]; then
+  for pid in data/redis-*/*.pid; do
+    kill $(cat $pid)
+  done
+  for pid in data/hive-*/*.pid; do
+    kill $(cat $pid)
+    rm $pid
+  done
+else
+  while getopts "hr:v:" OPT; do
+    case $OPT in
+    r)
+      REDIS=$(( 6000 + $OPTARG ))
+      kill $(cat data/redis-$REDIS/redis-$REDIS.pid)
+      ;;
+    v)
+      CONTROL=$(( 3000 + $OPTARG ))
+      kill $(cat data/hive-$CONTROL/hive.pid)
+      rm data/hive-$CONTROL/hive.pid
+      ;;
+    h)
+      echo "Usage $0 [-rREDIS|-vHIVE]"
+      ;;
+    *)
+      echo >2 Unknown option $OPT
+      exit 1
+      ;;
+    esac
+  done
+fi
+
+
