977b2397b2e56f3e3f83af82d76d8fc6551a02fc
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2020-12-19 18:12:38 -0600

    debug dns in gitlab ci

--- test/DnsLookupTest.cpp
+++ test/DnsLookupTest.cpp
@@ -7,8 +7,8 @@ void DnsLookupTest::testLocalhost()
   std::vector<std::string> ips;
 
   assert_true(dns.lookup("localhost", ips));
-  assert_equal(1, ips.size());
-  assert_equal("127.0.0.1", ips[0]);
+//  assert_equal(1, ips.size());
+  assert_equal("127.0.0.1", ips[0] + " " + ips[1]);
 }
 
 void DnsLookupTest::testInvalid()
