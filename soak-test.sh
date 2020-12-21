108b178a4791464c2210c34aed5ee8a02a655101
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2020-12-20 20:26:27 -0600

    add a "soak test" to stress the hive over a longer period

---
+++ soak-test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+helm upgrade redis-hive ./helm/redis-hive/ --install --force
+
+iter=0
+prev=
+errors=0
+
+while true; do
+  (( ++iter ))
+  echo >&2
+  echo >&2 Iteration $iter
+
+  replicas=$(kubectl exec deployment/redis-hive -credis -- redis-cli -h redis-hive-master info | grep connected_slaves | tr -d '\r')
+
+  if [[ $replicas != "connected_slaves:2" ]]; then
+    echo >&2 "FATAL: expected two replicas, got '$replicas'"
+    exit 1
+  fi
+
+  if [[ ! -z $prev ]]; then
+    for key in {a..z}; do
+      val=$(kubectl exec deployment/redis-hive -credis -- redis-cli -h redis-hive-replica get soak/$key)
+      if [[ $val != $prev ]]; then
+        echo >&2 "Error: soak/$key expected $prev, actual $val"
+        (( ++errors ))
+      fi
+    done
+  fi
+  echo >&2 "Total errors $errors"
+
+  now=$(date)
+
+  echo Setting keys to $now
+
+  for key in {a..z}; do
+    a=$(kubectl exec deployment/redis-hive -credis -- redis-cli -h redis-hive-master set soak/$key "$now")
+    if [[ $a != "OK" ]]; then
+      echo >&2 "$a"
+    fi
+  done
+
+  oldest=$(kubectl get pod -lapp=redis-hive --sort-by=.status.startTime  -ojsonpath={.items[0].metadata.name})
+  kubectl delete pod $oldest
+
+  sleep 10
+done
