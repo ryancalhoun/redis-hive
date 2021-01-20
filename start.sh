c144c7fcc347d8ed6eb22cfb3c715b8a198524a9
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2021-01-20 10:38:19 -0600

    remove all references to auth if not set

--- start.sh
+++ start.sh
@@ -11,14 +11,22 @@ for i in $(seq 1 $N); do
   mkdir -p data/redis-$REDIS
 
   if [[ ! -f data/redis-$REDIS/redis-$REDIS.pid ]]; then
-    redis-server <(sed 's/{{PORT}}/'$REDIS'/g;s/{{PASS}}/"'$REDIS_AUTH'"/g' redis.conf.in)
+    if [[ -z $REDIS_AUTH ]]; then
+      redis-server <(sed 's/{{PORT}}/'$REDIS'/g' redis.conf.in | grep -v '{{PASS}}')
+    else
+      redis-server <(sed 's/{{PORT}}/'$REDIS'/g;s/{{PASS}}/'$REDIS_AUTH'/g' redis.conf.in)
+    fi
   fi
 
   mkdir -p data/hive-$CONTROL
 
   if [[ ! -f data/hive-$CONTROL/hive.pid ]] ||
      [[ $(cat /proc/$(cat data/hive-$CONTROL/hive.pid)/comm 2>/dev/null) != redis-hive ]]; then
-    ./bin/redis-hive -r$REDIS -p$PROXY -aREDIS_AUTH -c$CONTROL -mlocalhost $(seq 3001 $(( 3000 + N)) | sed 's/^/-o/') >> data/hive-$CONTROL/hive.log 2>&1 &
+    if [[ -z $REDIS_AUTH ]]; then
+      ./bin/redis-hive -r$REDIS -p$PROXY -c$CONTROL -mlocalhost $(seq 3001 $(( 3000 + N)) | sed 's/^/-o/') >> data/hive-$CONTROL/hive.log 2>&1 &
+    else
+      ./bin/redis-hive -r$REDIS -p$PROXY -aREDIS_AUTH -c$CONTROL -mlocalhost $(seq 3001 $(( 3000 + N)) | sed 's/^/-o/') >> data/hive-$CONTROL/hive.log 2>&1 &
+    fi
     echo $! > data/hive-$CONTROL/hive.pid
   fi
 done
